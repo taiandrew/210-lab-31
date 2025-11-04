a600fa7fa7593bcb1344f12f76f87f97533c3765
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-11-04 08:51:52 -0800

    files

---
+++ main.cpp
@@ -0,0 +1,6 @@
+// COMSC 210 | Lab 29-31 | 4 NOV 2025
+// IDE: VS Code
+
+# include <iostream>
+# include <fstream>
+
---
+++ pasenger.h
@@ -0,0 +1,14 @@
+#ifndef PASSENGER_H
+#define PASSENGER_H
+
+#include <string>
+
+class Passenger {
+    
+private:
+
+public:
+
+};
+
+#endif
