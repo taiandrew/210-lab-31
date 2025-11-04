4891227accf9de9728775aa0edb58d45ae004253
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-11-04 09:05:53 -0800

    Reading file

--- main.cpp
+++ main.cpp
@@ -3,4 +3,33 @@
 
 # include <iostream>
 # include <fstream>
+# include <list>
 
+# include "passenger.h"
+
+using namespace std;
+
+// Constants
+const string FILEPATH = "/Users/andrewtai/Desktop/COMSC_210/projects/210-lab-31/passengers.txt";
+
+static list<Passenger> passengers;
+
+
+// MAIN
+int main() {
+
+    // Load passengers from file
+    ifstream inFile;
+    inFile.open(FILEPATH);
+    if (!inFile) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+    string name, queueType;
+    while (inFile >> name >> queueType) {
+        passengers.emplace_back(name, queueType);
+    }
+    inFile.close();
+
+    return 0;
+}
